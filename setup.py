31717956f792f4066893a6a05d49dff10f5d01d1
FlavioLionelRita <flaviolrital@hotmail.com>
2021-04-02 15:22:08 +0200

    set configuracion pipy

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup
+with open('README.md') as f:
+    long_description = f.read()
 setup(
   name = 'py-expression',
   packages = ['py_expression'],
-  version = '0.0.6',
+  version = '0.0.7',
   description = 'parser and solve expressions',
-  author = 'Flavio Lionel Rita',
-  author_email = 'flaviolrita@hotmail.com',
+  long_description=long_description,
+  long_description_content_type='text/markdown',  # This is important!
   url = 'https://github.com/FlavioLionelRita/py-expression', # use the URL to the github repo
   download_url = 'https://github.com/FlavioLionelRita/py-expression/tarball/0.0.1',
   keywords = ['parser', 'expression'],
   classifiers = [],
+  author = 'Flavio Lionel Rita',
+  author_email = 'flaviolrita@hotmail.com'  
 )
