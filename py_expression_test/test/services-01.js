7fd765cdef36dc86518f71a7b2c09ad6c183774c
Flavio Lionel Rita <43414577+FlavioLionelRita@users.noreply.github.com>
2021-05-10 11:16:11 +0200

    add pending features

---
+++ py_expression_test/test/services-01.js
@@ -0,0 +1,7 @@
+//TODO: resolver metodos asincronicos
+
+result=await rest(url,action,body,headers,cookies); 
+
+result=await rest(url(urlJoin($HOST,'/user/'+id),args),'GET',null,['context-type=json']);
+
+result=await soap(url,action,body,headers,cookies); 
